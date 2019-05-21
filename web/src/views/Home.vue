5645a6871613345fec5843bd06fec17841e594e6
wxs77577 <wu-xuesong@qq.com>
2019-05-21 22:22:38 +0800

    使用字体图标 (iconfont)

--- web/src/views/Home.vue
+++ web/src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <i class="iconfont icon-menu"></i>
   </div>
 </template>
 
